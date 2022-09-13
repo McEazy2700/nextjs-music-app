12fac634faa64da5a6da2c14bf77ecf98d27e2c3
Ezekiel Victor <codeepoch@gmail.com>
2022-09-13 08:14:41 +0000

    Completed search bar component🔎

--- client/components/atoms/a-search/SearchBar.tsx
+++ client/components/atoms/a-search/SearchBar.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiSearchAlt } from 'react-icons/bi'
 import styles from './Search.module.scss'
+import Gstyles from '../../../styles/Universal.module.scss'
+
+interface Classes{
+  classes: string;
+}
 
 const SearchBar = () => {
+  const [classes, setClasses] = useState<string>(styles.searchBar)
+  const inputFocusHandler = ()=>{
+    setClasses(curr => `${curr} ${Gstyles.shadow}`)
+  }
+
+  const inputBlurHandler = ()=>{
+    setClasses(styles.searchBar)
+  }
+  
   return (
-    <div className={styles.searchBar}>
-        <label className={styles.label} htmlFor="search"><BiSearchAlt /></label>
-        <input className={styles.input} type="text" id='search' placeholder='Search for artists and songs'/>
+    <div className={classes}>
+        <label className={styles.label} htmlFor="search">
+          <button className={styles.button} type="submit">
+            <BiSearchAlt />
+          </button>
+        </label>
+        <input onFocus={inputFocusHandler} 
+        onBlur={inputBlurHandler}
+        className={styles.input} type="text" id='search' placeholder='Search for artists and songs'/>
     </div>
   )
 }
