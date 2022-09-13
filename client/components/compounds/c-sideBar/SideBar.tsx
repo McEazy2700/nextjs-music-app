407be6ba0b8b6c39b60a1e0f4411319bb1e6919c
Ezekiel Victor <codeepoch@gmail.com>
2022-09-13 11:24:07 +0000

    started with navigaion bar

--- client/components/compounds/c-sideBar/SideBar.tsx
+++ client/components/compounds/c-sideBar/SideBar.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import styles from './SideBar.module.scss'
+import Image from 'next/image'
+import { HiHome } from 'react-icons/hi'
+import { BsMusicNote } from 'react-icons/bs'
+import { RiUserVoiceFill } from 'react-icons/ri'
+import { CgPlayListSearch } from 'react-icons/cg'
+import { SiDiscogs } from 'react-icons/si'
+import NavItem from '../../atoms/a-search/a-navItem/NavItem'
+
 
 const SideBar = () => {
   return (
-    <div className={styles.bar}>SideBar</div>
+    <nav className={styles.nav}>
+      <div>
+        <div className={styles.logoContainer}>
+          <SiDiscogs className={styles.logoItem}/>
+          <SiDiscogs className={styles.logoItem}/>
+          <SiDiscogs className={styles.logoItem}/>
+        </div>
+        {/* <Image src='/Logo.svg' width='60' height='40'/> */}
+      </div>
+      <div className={styles.navItems}>
+        <NavItem navIcon={HiHome}/>
+        <NavItem navIcon={BsMusicNote} />
+        <NavItem navIcon={RiUserVoiceFill} />
+        <NavItem navIcon={CgPlayListSearch} />
+      </div>
+    </nav>
   )
 }
 
